4028d95978ae0fd156d7f72049f03c27ea5c9cf8
eugenest <eugene.timofieiev@gmail.com>
2015-02-17 09:56:12 +0000

    cleanup

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2notes',
     'username' => 'root',
-    'password' => 'zzajujc1',
+    'password' => 'test',
     'charset' => 'utf8',
 ];
