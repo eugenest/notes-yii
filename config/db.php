81de97481ba20942f1bcf88c57c7e5a3ef3890b6
eugenest <eugene.timofieiev@gmail.com>
2015-02-17 12:29:44 +0000

    del config/db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2notes',
-    'username' => 'root',
-    'password' => 'test',
-    'charset' => 'utf8',
-];
